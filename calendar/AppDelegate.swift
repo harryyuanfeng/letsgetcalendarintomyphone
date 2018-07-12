0f1efbd5b48c66c311193338d356eeac2a2a1ecb
harryfeng <2307555175@qq.com>
2018-07-11 23:42:07 -0400

    Initial Commit

---
+++ calendar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  calendar
+//
+//  Created by harryfeng on 2018-07-11.
+//  Copyright © 2018 harryfeng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
