bc7aca06b8be631cf0f611751df4a5eb85ed23a8
harryfeng <2307555175@qq.com>
2018-07-12 23:46:33 -0400

    almost finish

--- calendar/AppDelegate.swift
+++ calendar/AppDelegate.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 import EventKit
-
+import Foundation
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -38,14 +38,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Something cool
         events = self.lcaldaar.getUpcomingEvents();
         let now = Date();
+        
         for event in events {
             let eventDate = event.startDate as Date;
             let difference = eventDate.timeIntervalSince(now);
             print(event.title);
             print(difference);
             print(difference < 300 && difference > 293);
+            if(difference < 300 && difference > 293){
+                let output = shell("curl -s  --form-string \"token=ap739k1h5j2dv4zjakk65jtz6m6zdb\"  --form-string \"user=uswhieh8gutmvasee6m7bt1oxc8ira\"  --form-string \"message=hello world\"  https://api.pushover.net/1/messages.json");
+                print(output);
+            }
         }
     }
+    
+    func shell(_ command: String) -> String {
+        let task = Process()
+        task.launchPath = "/bin/bash"
+        task.arguments = ["-c", command]
+        
+        let pipe = Pipe()
+        task.standardOutput = pipe
+        task.launch()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output: String = NSString(data: data, encoding: String.Encoding.utf8.rawValue)! as String
+        
+        return output
+    }
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
